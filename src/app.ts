dd8f6fc0c55777d10e94c8544991bdab6ec30c9d
Razak <moabdulrazak17@gmail.com>
2022-09-15 01:57:07 +0000

    chore: simple fastify server

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,17 @@
-console.log('Hello Mom!');
+import Fastify from 'fastify';
+
+const fastify = Fastify({
+	logger: true
+});
+
+fastify.get('/health', async (request, reply) => {
+	return { status: 'OK' };
+});
+
+fastify.listen({ port: 3000, host: '0.0.0.0' }, function (err, address) {
+	if (err) {
+		fastify.log.error(err);
+		process.exit(1);
+	}
+	fastify.log.info(`server listening on ${address}`);
+});
